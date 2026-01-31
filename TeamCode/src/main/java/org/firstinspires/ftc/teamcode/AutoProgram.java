57eec62d4328c14312e56d2858397f0c65fe96b3
FTC-FL <fortloramieroboticsclub@gmail.com>
2026-01-31 16:43:41 -0500

    Config fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoProgram.java
@@ -11,6 +11,7 @@ import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Mechanisms.IntakeTest;
